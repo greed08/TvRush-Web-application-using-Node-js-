051b4981bbee2685e49073b67ff8aff8c2c63974
Omkar8039 <Omilpsablock@gmail.com>
2017-06-13 21:27:29 +0530

    Like/Dislike and other  functinality added

--- routes/search.js
+++ routes/search.js
@@ -34,13 +34,13 @@ router.post('/response',function(req,res)
      let torm=data.tv_or_movie;
      let overview=data.overview;
      let img_src=data.img_src;
-     console.log("Title of the liked attribute is "+title);
-     console.log("You selected "+torm);
-     console.log("Overview is "+overview);
-     console.log("Image souce is "+img_src);
+     //console.log("Title of the liked attribute is "+title);
+     //console.log("You selected "+torm);
+     //console.log("Overview is "+overview);
+     //console.log("Image souce is "+img_src);
      if(torm==='Movies'){
        //console.log('You selected movies and likes');
-       User.find({likeMovie:img_src},function(err,user)
+       User.find({username:req.session.user,likeMovie:img_src},function(err,user)
      {
         if(!err)
         {
@@ -99,6 +99,131 @@ router.post('/response',function(req,res)
 }
 });
 }
+/*Here like option selected but selected tv series*/
+
+else if(torm!=='Movies')
+{
+
+
+
+console.log(title);
+console.log(img_src);
+  
+
+       User.find({username:req.session.user,likeTv:img_src},function(err,user)
+     {
+        if(!err)
+        {
+          if(user==undefined||user.length<1)
+          {
+          console.log('Data in tv not present');
+          let query={username:req.session.user};
+          let update={$addToSet:{likeTv:img_src}};
+          let options={multi:true};
+
+          User.update(query,update,options,callback);
+          function callback(err,numaffected)
+          {
+            if(!err)
+            {
+               console.log("Number of rows affected in tv are "+numaffected["nRemoved"]);
+               msg="You liked "+title+"!!";
+             }
+             else {
+               util.log(err);
+               msg="Error occured";
+             }
+             res.json({
+               success:true,
+               msg:msg
+             });
+          }
+        }
+          else {
+            console.log("Data already present in tvlikes");
+            let query={username:req.session.user};
+            let update={$pull:{likeTv:img_src}};
+            let options={multi:true};
+            User.update(query,update,options,callback);
+            function callback(err,numaffected)
+            {
+              if(!err)
+              {
+                console.log("Number of rows deleted in tv are "+numaffected["nRemoved"]);
+                 msg="You disliked "+title+"!!";
+               }
+               else {
+                 util.log(err);
+                 msg="Error occured";
+               }
+               res.json({
+                 success:true,
+                 msg:msg
+               });
+
+          }
+        }
+
+
+
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
@@ -115,12 +240,385 @@ router.post('/response',function(req,res)
 
    if(data.eve==='add_to_wishlist')
       {
+     let title=data.title;
+     let torm=data.tv_or_movie;
+     let overview=data.overview;
+     let img_src=data.img_src;
+      if(torm==='Movies')
+      {
+    User.find({username:req.session.user,wishlistMovie:img_src},function(err,user)
+     {
+        if(!err)
+        {
+          if(user==undefined||user.length<1)
+          {
+          console.log('Data in tv not present');
+          let query={username:req.session.user};
+          let update={$addToSet:{wishlistMovie:img_src}};
+          let options={multi:true};
 
-        msg='Added '+data.title+' to your wishlist!!';
+          User.update(query,update,options,callback);
+          function callback(err,numaffected)
+          {
+            if(!err)
+            {
+               console.log("Number of rows affected in tv are "+numaffected["nRemoved"]);
+               msg="You added "+title+" to watch list!";
+             }
+             else {
+               util.log(err);
+               msg="Error occured";
+             }
+             res.json({
+               success:true,
+               msg:msg
+             });
+          }
+        }
+          else {
+            console.log("Data already present in tvlikes");
+            let query={username:req.session.user};
+            let update={$pull:{wishlistMovie:img_src}};
+            let options={multi:true};
+            User.update(query,update,options,callback);
+            function callback(err,numaffected)
+            {
+              if(!err)
+              {
+                console.log("Number of rows deleted in tv are "+numaffected["nRemoved"]);
+                 msg="You removed  "+title+" from watch list!";
+               }
+               else {
+                 util.log(err);
+                 msg="Error occured";
+               }
+               res.json({
+                 success:true,
+                 msg:msg
+               });
+
+          }
+        }
+
+
+
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      }
+      else if(torm!=='Movies')
+      {
+          User.find({username:req.session.user,wishlistTv:img_src},function(err,user)
+     {
+        if(!err)
+        {
+          if(user==undefined||user.length<1)
+          {
+          console.log('Data in tv not present');
+          let query={username:req.session.user};
+          let update={$addToSet:{wishlistTv:img_src}};
+          let options={multi:true};
+
+          User.update(query,update,options,callback);
+          function callback(err,numaffected)
+          {
+            if(!err)
+            {
+               console.log("Number of rows affected in tv are "+numaffected["nRemoved"]);
+               msg="You added "+title+" to watch list!";
+             }
+             else {
+               util.log(err);
+               msg="Error occured";
+             }
+             res.json({
+               success:true,
+               msg:msg
+             });
+          }
+        }
+          else {
+            console.log("Data already present in tvlikes");
+            let query={username:req.session.user};
+            let update={$pull:{wishlistTv:img_src}};
+            let options={multi:true};
+            User.update(query,update,options,callback);
+            function callback(err,numaffected)
+            {
+              if(!err)
+              {
+                console.log("Number of rows deleted in tv are "+numaffected["nRemoved"]);
+                 msg="You removed  "+title+" from watch list!";
+               }
+               else {
+                 util.log(err);
+                 msg="Error occured";
+               }
+               res.json({
+                 success:true,
+                 msg:msg
+               });
+
+          }
+        }
+
+
+
+}
+});
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       }
    if(data.eve==='add_to_watchlist')
       {
-        msg='Added '+data.title+ ' to your watched history!!';
+     let title=data.title;
+     let torm=data.tv_or_movie;
+     let overview=data.overview;
+     let img_src=data.img_src;
+       if(torm==='Movies')
+       {
+        console.log('Watched history movie selected');
+         
+      User.find({username:req.session.user,watchlistMovie:img_src},function(err,user)
+     {
+        if(!err)
+        {
+          if(user==undefined||user.length<1)
+          {
+          console.log('Data not present');
+          let query={username:req.session.user};
+          let update={$addToSet:{watchlistMovie:img_src}};
+          let options={multi:true};
+
+          User.update(query,update,options,callback);
+          function callback(err,numaffected)
+          {
+            if(!err)
+            {
+              //console.log("Number os rows affected are "+numaffected["nRemoved"]);
+               msg="You watched "+title+"!";
+             }
+             else {
+               util.log(err);
+               msg="Errro occured";
+             }
+             res.json({
+               success:true,
+               msg:msg
+             });
+          }
+        }
+          else {
+            console.log("Data already present");
+            let query={username:req.session.user};
+            let update={$pull:{watchlistMovie:img_src}};
+            let options={multi:true};
+            User.update(query,update,options,callback);
+            function callback(err,numaffected)
+            {
+              if(!err)
+              {
+                //console.log("Number os rows affected are "+numaffected["nRemoved"]);
+                 msg="You removed "+title+" from watched history!";
+               }
+               else {
+                 util.log(err);
+                 msg="Errro occured";
+               }
+               res.json({
+                 success:true,
+                 msg:msg
+               });
+
+          }
+        }
+
+
+
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       }
+       else if(torm!=='Movies')
+       {
+
+        
+       //console.log('Watched history tv selected');
+         User.find({username:req.session.user,watchlistTv:img_src},function(err,user)
+     {
+        if(!err)
+        {
+          if(user==undefined||user.length<1)
+          {
+          console.log('Data not present');
+          let query={username:req.session.user};
+          let update={$addToSet:{watchlistTv:img_src}};
+          let options={multi:true};
+
+          User.update(query,update,options,callback);
+          function callback(err,numaffected)
+          {
+            if(!err)
+            {
+              //console.log("Number os rows affected are "+numaffected["nRemoved"]);
+               msg="You watched "+title+"!";
+             }
+             else {
+               util.log(err);
+               msg="Errro occured";
+             }
+             res.json({
+               success:true,
+               msg:msg
+             });
+          }
+        }
+          else {
+            console.log("Data already present");
+            let query={username:req.session.user};
+            let update={$pull:{watchlistTv:img_src}};
+            let options={multi:true};
+            User.update(query,update,options,callback);
+            function callback(err,numaffected)
+            {
+              if(!err)
+              {
+                //console.log("Number os rows affected are "+numaffected["nRemoved"]);
+                 msg="You removed "+title+" from watched history!";
+               }
+               else {
+                 util.log(err);
+                 msg="Errro occured";
+               }
+               res.json({
+                 success:true,
+                 msg:msg
+               });
+
+          }
+        }
+
+
+
+}
+});
+
+
+
+       }
+
+       
       }
 
     }
